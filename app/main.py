4f9264aaf32f670ffd2226055020373098420f81
RUYANGA <ruyangam15@gmail.com>
2026-07-02 17:07:53 +0200

    feat: add update task endpoint

--- app/main.py
+++ app/main.py
@@ -33,3 +33,19 @@ def create_task(task: TaskCreate):
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated: TaskCreate):
+
+    for task in tasks:
+        if task.id == task_id:
+            task.title = updated.title
+
+            logger.info(f"Task {task_id} updated")
+
+            return task
+
+    logger.error(f"Task {task_id} not found")
+    raise HTTPException(status_code=404, detail="Task not found")
